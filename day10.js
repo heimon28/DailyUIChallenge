6169592fcb804c12dd832a53cb95e508922c1bce
heimon1255@gmail.com <heimon28@gmail.com>
2024-02-19 19:45:09 +0530

    added some practice projects

---
+++ day10.js
@@ -0,0 +1,7 @@
+let shareBtn = document.querySelector(".share-Btn");
+shareBtn.addEventListener("click",()=>{
+    shareBtn.style.background="white";
+    shareBtn.innerText="Done";
+    shareBtn.style.color="black";
+
+})
